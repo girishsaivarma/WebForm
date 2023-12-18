8a50ed41f3e747e699d5621d62140f2986af609a
girishsaivarma <151422355+girishsaivarma@users.noreply.github.com>
2023-12-17 21:14:15 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Install or update NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+# Source NVM in the current shell
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
+
+# Install Node.js 16.x
+nvm install 16
+
+# Use Node.js 16.x
+nvm use 16
+
+# Install Newman
+npm install -g newman
+
+# Install datetime
+pip3 install datetime
+
+# Install secrets
+pip3 install secrets
+
+
+# Update pip and setup tools
+pip3 install --upgrade pip setuptools
